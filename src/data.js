3ec854e243472071c34f880e2f3f36691b6cbf51
Yolanda Ribas <yolanda.ribas@hotmail.com>
2019-02-20 09:37:16 -0600

    start to create a function to show pokemons

--- src/data.js
+++ src/data.js
@@ -1,8 +1,58 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
+//const example = () => {
+//  return 'example';
+//};
+//window.example = example;
 
-const example = () => {
-  return 'example';
+let card = document.querySelector('#content');
+let html = '';
+
+function PokemonTemplate(singlePokemon){
+    let pokemonCardTemplate = `
+    <div>
+      <h1>${singlePokemon.name}</h1> 
+      <h1> N. ${singlePokemon.num}</h1>
+      <img src="${singlePokemon.img}">       
+    </div>
+    `;
+    return pokemonCardTemplate;
 };
 
-window.example = example;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+card.innerHTML = html;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
