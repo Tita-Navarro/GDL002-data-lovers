459b91419859aade579ff10d62af8141a86d3e3a
Yolanda Ribas <yolanda.ribas@hotmail.com>
2019-02-21 09:15:34 -0600

    add functions to use button home and pokemon list

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-// Funcion para controlar pantallas
+// Declaraciones para controlar pantallas
+let idPantallaActual = "root";
+let pantallaInicial = document.getElementById(idPantallaActual);
+pantallaInicial.style.display = "block";
+
+const list = document.getElementById('list');
+const home = document.getElementById('home');
+list.onclick = ()=> controladorPantallas('screen_1');
+home.onclick = ()=> controladorPantallas('root');
+
+// Funcion que controla pantallas
 const controladorPantallas = (idPantallaNueva) => {
     const pantallaActual = document.getElementById(idPantallaActual);
     const pantallaNueva = document.getElementById(idPantallaNueva); 
@@ -7,5 +17,4 @@ const controladorPantallas = (idPantallaNueva) => {
     idPantallaActual = idPantallaNueva;
   };
 
-  const btn1 = document.getElementById('list');
-  btn1.onclick = ()=> controladorPantallas('screen_1');
+ 
