630cb2aa5d4acda55bb996bcdc32631a34756799
Tita Navarro <45797993+Tita-Navarro@users.noreply.github.com>
2019-02-25 23:07:06 -0600

    commit del maquetado de proyecto

--- src/data.js
+++ src/data.js
@@ -1,22 +1,27 @@
+
 let card = document.querySelector('#pkList');
 let html = '';
 
 function pokemonTemplate(singlePokemon){
     let pokemonCardTemplate = `
-    <div class="singlePokemon">
-      <h1>${singlePokemon.name}</h1>
+    <div id="info" class="singlePokemon">
+      <h1 class="namepk">${singlePokemon.name}</h1>
       <img src="${singlePokemon.img}">        
     </div>
     `;
     return pokemonCardTemplate;
 }
-
 for(let singlePokemon of POKEMON.pokemon) {
     html = html + pokemonTemplate(singlePokemon); //Es para que junte las tarjetitas cada vez q se cumpla el ciclo, para que las vaya sumando en pantalla
   }    
     
 card.innerHTML = html;
 
+/*window.data = {
+  const alphabetPokemon = () => {
+console.log(POKEMON.pokemon.sort);
+}
+  };*/
 
 
 
--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 // Declaraciones para controlar pantallas
   const list=()=>{
     document.getElementById ("pkList").style.display= 'flex';
+    document.getElementById ("lateral_content").style.display="block";
     document.getElementById ("root").style.display='none';
   };
   document.getElementById("list").addEventListener('click', list);
 
   const home=()=>{
     document.getElementById("root").style.display='block';
+    document.getElementById ("lateral_content").style.display="none";
     document.getElementById("pkList").style.display='none';
   };
 document.getElementById("home").addEventListener('click', home);
